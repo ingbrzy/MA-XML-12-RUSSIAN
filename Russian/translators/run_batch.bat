1f523739f96bf4249def1a947d4562809ac6b125
mclaude74 <45774262+mclaude74@users.noreply.github.com>
2020-08-20 18:49:42 +0300

    added stable devices + repo

---
+++ Russian/translators/run_batch.bat
@@ -0,0 +1,24 @@
+cls
+;
+call routine.bat chiron Settings.apk 
+call routine.bat jason Settings.apk 
+call routine.bat sagit Settings.apk 
+call routine.bat wayne Settings.apk 
+call routine.bat whyred Settings.apk 
+;
+call routine.bat cmi Settings.apk 
+call routine.bat umi Settings.apk 
+;
+call routine.bat cepheus_stable Settings.apk 
+call routine.bat crux_stable Settings.apk 
+call routine.bat davinci_stable Settings.apk 
+call routine.bat phoenix_stable Settings.apk 
+call routine.bat picasso_stable Settings.apk 
+call routine.bat raphael_stable Settings.apk 
+;
+call routine.bat lmi_stable Settings.apk 
+call routine.bat cmi_stable Settings.apk 
+call routine.bat umi_stable Settings.apk 
+;
+call routine.bat cmi MiuiCamera.apk 
+call routine.bat umi MiuiCamera.apk 
