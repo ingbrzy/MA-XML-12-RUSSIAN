b014f1aa9e2c8996d99726744565a9f90743f66e
mclaude74 <45774262+mclaude74@users.noreply.github.com>
2020-08-24 18:07:57 +0300

    tune
    * Цвета и контрастность -> Цветовой тон и контрастность
    * прочее

---
+++ Russian/translators/run_copy_default.bat
@@ -0,0 +1,27 @@
+SET _DEVICE=%1
+SET _APKFILE=%2
+
+SET _src=D:\Android\Github\MA-XML-12-Compare\%_DEVICE%\%_APKFILE%.apk\res\values\
+SET _dst=D:\Android\Github\MA-XML-12-RUSSIAN\Russian\main\%_APKFILE%.apk\res\values-ru\
+
+IF EXIST %_src%strings.xml (
+  copy %_src%strings.xml D:\Android\XMLCompare\SRC 
+  copy %_dst%strings.xml D:\Android\XMLCompare\DST 
+) ELSE (
+  echo FAILED
+  goto _failed
+)
+
+XMLCompare /s D:\Android\XMLCompare\SRC\strings.xml /d D:\Android\XMLCompare\DST\strings.xml /i D:\Android\XMLCompare\ignore.xml /t D:\Android\XMLCompare\TRS\strings.xml /n 4
+
+IF EXIST .\DST\result.xml (
+  copy .\DST\result.xml .\DST\strings.xml
+  del .\DST\result.xml
+
+  copy .\DST\strings.xml %_dst%
+) ELSE (
+  echo FAILED
+  goto _failed
+)
+
+:_failed
