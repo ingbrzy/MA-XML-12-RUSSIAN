816340baf239a99fde4f47eae2ad16d4288c6761
mclaude74 <45774262+mclaude74@users.noreply.github.com>
2020-08-28 17:28:46 +0300

    update batch

--- Russian/translators/run_copy_translation.bat
+++ Russian/translators/run_copy_translation.bat
@@ -5,6 +5,7 @@ SET _EXT=%3
 SET _run=D:\Android\XMLCompare\
 SET _src=D:\Android\Github\MA-XML-12-Compare\%_DEVICE%\%_APKFILE%.apk\res\values\
 SET _dst=D:\Android\Github\MA-XML-12-RUSSIAN\Russian\main\%_APKFILE%.apk\res\values-ru%_EXT%\
+SET _dat=D:\Android\Github\MA-XML-12-RUSSIAN\Russian\translators\
 
 IF EXIST %_src%strings.xml (
   copy %_src%strings.xml D:\Android\XMLCompare\SRC 
@@ -14,11 +15,11 @@ IF EXIST %_src%strings.xml (
   goto _failed
 )
 
-%_run%XMLCompare /s %_run%SRC\strings.xml /d %_run%DST\strings.xml /i %_run%ignore.xml /t %_run%translate.xml /n 4 /silent_t
+%_run%XMLCompare /s %_run%SRC\strings.xml /d %_run%DST\strings.xml /i %_dat%ignore.xml /t %_dat%translate.xml /n 4 /silent_t
 
 IF EXIST %_run%DST\result.xml (
   copy %_run%DST\result.xml %_run%DST\strings.xml
-`  copy %_run%DST\strings.xml %_dst%
+  copy %_run%DST\strings.xml %_dst%
 ) ELSE (
   echo FAILED
   goto _failed
