1f2d773c0b4c0eca3b1d09f614745bbbcae06333
mclaude74 <mclaude@inbox.ru>
2020-10-26 00:41:14 +0300

    update batch & utility + add devices

--- Russian/translators/run_batch.bat
+++ Russian/translators/run_batch.bat
@@ -1,12 +1,18 @@
 cls
 ;
 call routine.bat chiron Settings.apk 
+call routine.bat davinci Settings.apk 
 call routine.bat jason Settings.apk 
+call routine.bat phoenix Settings.apk 
+call routine.bat picasso Settings.apk 
+call routine.bat picasso_48m Settings.apk 
 call routine.bat sagit Settings.apk 
+call routine.bat tucana Settings.apk 
 call routine.bat wayne Settings.apk 
 call routine.bat whyred Settings.apk 
 ;
 call routine.bat cmi Settings.apk 
+call routine.bat lmi Settings.apk 
 call routine.bat umi Settings.apk 
 ;
 call routine.bat cepheus Settings.apk 
@@ -29,7 +35,7 @@ call routine.bat vela Settings.apk
 ;
 call routine.bat cmi MiuiCamera.apk 
 call routine.bat umi MiuiCamera.apk 
-;                         ;
+;                         
 call routine.bat cas MiuiSystemUI.apk 
 call routine.bat cmi MiuiSystemUI.apk 
 call routine.bat lavender MiuiSystemUI.apk 
