24f23244c01101dfbcb68d215da4adedeec42f65
mclaude74 <mclaude@inbox.ru>
2020-11-09 02:55:11 +0300

    update devices

--- Russian/translators/run_batch.bat
+++ Russian/translators/run_batch.bat
@@ -4,33 +4,37 @@ cls
 del D:\Android\XMLCompare\report.txt
 @echo on
 ;
-call routine.bat chiron Settings.apk 
+;Android 11 
+;
+call routine.bat cas Settings.apk 
+call routine.bat cmi Settings.apk 
 call routine.bat davinci Settings.apk 
-call routine.bat jason Settings.apk 
+call routine.bat lmi Settings.apk 
 call routine.bat phoenix Settings.apk 
 call routine.bat picasso Settings.apk 
 call routine.bat picasso_48m Settings.apk 
-call routine.bat sagit Settings.apk 
-call routine.bat tucana Settings.apk 
-call routine.bat wayne Settings.apk 
-call routine.bat whyred Settings.apk 
-;
-call routine.bat cmi Settings.apk 
-call routine.bat lmi Settings.apk 
 call routine.bat umi Settings.apk 
-;
-call routine.bat cepheus Settings.apk 
-call routine.bat dipper Settings.apk 
-call routine.bat lavender Settings.apk 
-call routine.bat cas Settings.apk 
-;
-call routine.bat vela Settings.apk 
+call routine.bat vangogh Settings.apk 
 ;
 call routine.bat cmi MiuiCamera.apk 
 call routine.bat umi MiuiCamera.apk 
 ;                         
 call routine.bat cas MiuiSystemUI.apk 
 call routine.bat cmi MiuiSystemUI.apk 
-call routine.bat lavender MiuiSystemUI.apk 
 call routine.bat lmi MiuiSystemUI.apk 
 call routine.bat umi MiuiSystemUI.apk 
+;
+; Android 10
+;
+call routine.bat cepheus Settings.apk 
+call routine.bat chiron Settings.apk 
+call routine.bat dipper Settings.apk 
+call routine.bat jason Settings.apk 
+call routine.bat lavender Settings.apk 
+call routine.bat sagit Settings.apk 
+call routine.bat tucana Settings.apk 
+call routine.bat vela Settings.apk 
+call routine.bat wayne Settings.apk 
+call routine.bat whyred Settings.apk 
+;
+call routine.bat lavender MiuiSystemUI.apk 
